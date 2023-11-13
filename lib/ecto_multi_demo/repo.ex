b89bf6dfb6f283bb69b05e908fa75947546ceb95
Nicholas Moen <arcanemachine@tutanota.com>
2023-11-12 19:21:51 -0700

    Create rudimentary 'count' function in repo module

--- lib/ecto_multi_demo/repo.ex
+++ lib/ecto_multi_demo/repo.ex
@@ -1,5 +1,11 @@
 defmodule EctoMultiDemo.Repo do
+  import Ecto.Query
+
   use Ecto.Repo,
     otp_app: :ecto_multi_demo,
     adapter: Ecto.Adapters.Postgres
+
+  def count(queryable) do
+    from(q in queryable, select: count(q.id)) |> __MODULE__.one()
+  end
 end
